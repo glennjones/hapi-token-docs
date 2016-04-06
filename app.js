f76b53e7b905bb5f6ac8f4e2c16a141f30c6193b
Glenn Jones <glennjonesnet@gmail.com>
2016-04-06 16:58:44 +0100

    auth.strategy in place

--- app.js
+++ app.js
@@ -3,13 +3,51 @@ const Hapi = require('hapi');
 const Inert = require('inert');
 const Vision = require('vision');
 const Blipp = require('blipp');
+const Hoek = require('hoek');
 const JWT = require('jsonwebtoken');
 const HapiJWT2 = require('hapi-auth-jwt2');
+const Bell = require('bell');
+const AuthCookie = require('hapi-auth-cookie');
 const Pack = require('./package');
 const Routes = require('./lib/routes.js');
 
 
-const  server = new Hapi.Server();
+const appendAuthMessages = function( routes ){
+
+    routes.forEach( (route) => {
+        if(Hoek.reach(route, 'config.tags')){
+            const tags = Hoek.reach(route, 'config.tags');
+            if(tags.indexOf('api') > -1){
+
+                if(!route.config.notes){
+                    route.config.notes = []
+                }
+
+                if(route.config.auth === 'jwt'){
+                    route.config.notes.push('__Token is required to access this endpoint. Make sure your sign in.__');
+                }
+            }
+        }
+    });
+}
+
+appendAuthMessages( Routes );
+
+
+const privateKey = process.env.PRIVATEKEY;
+const info = {
+        version: Pack.version,
+        title: 'Hapi - token - docs',
+        description: 'This web API was built to demonstrate some of the hapi features and functionality.'
+    }
+
+
+const server = new Hapi.Server({
+    app: {
+        'privateKey': privateKey,
+        'info': info,
+    }
+});
 server.connection({
     host: (process.env.PORT) ? '0.0.0.0' : 'localhost',
     port: (process.env.PORT || 3033),
@@ -17,41 +55,9 @@ server.connection({
 });
 
 
-
-
-var people = { // our "users database"
-    56732: {
-      id: 56732,
-      name: 'Jen Jones',
-      scope: ['a', 'b']
-    }
-};
-
-var privateKey ='hapi hapi joi joi';
-var token = JWT.sign({ id: 56732 }, privateKey, { algorithm: 'HS256'} );
-
-// bring your own validation function
-var validate = function (decoded, request, callback) {
-
-    // do your checks to see if the person is valid
-    if (!people[decoded.id]) {
-      return callback(null, false);
-    }
-    else {
-      return callback(null, true, people[decoded.id]);
-    }
-};
-
-
-
-
 // setup swagger options
 const swaggerOptions = {
-    info: {
-        version: Pack.version,
-        title: 'Hapi - token - docs',
-        description: 'This web API was built to demonstrate some of the hapi features and functionality.'
-    },
+    info: info,
     tags: [{
         'name': 'sum',
         'description': 'Working with maths',
@@ -74,11 +80,28 @@ const swaggerOptions = {
             'in': 'header'
         }
     },
-    security: [{ 'jwt': [] }]
+    security: [{ 'jwt': [] }],
+    auth: false
 };
+
+
+//  validation function for JWT token
+var validate = function (decoded, request, callback) {
+
+    // add yours checks here i.e. is the user valid
+    if (decoded.id && decoded.name) {
+        return callback(null, true, decoded);
+    } else {
+        return callback(null, false);
+    }
+};
+
+
 
 // register plug-ins
 server.register([
+    AuthCookie,
+    Bell,
     HapiJWT2,
     Inert,
     Vision,
@@ -93,25 +116,30 @@ server.register([
             console.log(err);
         }
 
+        server.auth.strategy('hapi-token-docs-cookie', 'cookie', {
+            password: process.env.COOKIE_PASSWORD,
+            cookie: 'hapi-token-docs',
+            isSecure: false
+        });
+
+        server.auth.strategy('github-oauth', 'bell', {
+            provider: 'github',
+            password: process.env.GITHUB_PASSWORD,
+            clientId: process.env.GITHUB_CLIENTID,
+            clientSecret: process.env.GITHUB_CLIENTSECRET,
+            isSecure: false
+        });
+
         server.auth.strategy('jwt', 'jwt', {
             key: privateKey,
             validateFunc: validate,
             verifyOptions: { algorithms: [ 'HS256' ] }
         });
 
+        server.auth.default('hapi-token-docs-cookie');
+
         // add routes
         server.route(Routes);
-        server.route({
-            method: 'GET',
-            path: '/token',
-            config: {
-                auth: false,
-                tags: ['api'],
-                handler: function(request, reply) {
-                    reply({token: token})
-                }
-            }
-        })
 
         server.start(function(){
             console.log('Server running at:', server.info.uri);
@@ -128,8 +156,3 @@ server.views({
     helpersPath: './templates/helpers',
     isCached: false
 })
-
-
-
-
-
